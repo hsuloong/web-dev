53bf467d6743da4d5d7255d3b410c5a729e7eb66
hsuloong <hsuloong@outlook.com>
2020-02-08 19:56:57 +0800

    add module

---
+++ Vue/learn-example/module/import.js
@@ -0,0 +1,4 @@
+import all from "./module.js"
+//import * as mdi from "./module.js"
+
+console.log(all)
---
+++ Vue/learn-example/module/module.js
@@ -0,0 +1,14 @@
+var name = "刘慈欣"
+var age = 18
+var flag = true
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+export default {
+  name,
+  age,
+  flag,
+  sum
+}
