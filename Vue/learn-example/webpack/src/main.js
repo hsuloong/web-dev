703e337ce9c13635d2dc4fed391ffb5425312270
hsuloong <hsuloong@outlook.com>
2020-02-08 22:12:31 +0800

    add webpack project example

---
+++ Vue/learn-example/webpack/src/main.js
@@ -0,0 +1,10 @@
+// import * as mathUtils from "./mathUtils.js"
+const {sum, mul} = require("./js/mathUtils.js")
+
+var name = "Vue"
+
+console.log(sum(10, 20))
+
+require('./css/normal.css')
+
+// webpack ./src/main.js -o ./dist/bundle.js --mode development
