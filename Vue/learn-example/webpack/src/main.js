7d8aa76191cc9070a099125eb2526eab1918f97a
hsuloong <hsuloong@outlook.com>
2020-02-09 00:23:29 +0800

    add .vue .less.... loader

--- Vue/learn-example/webpack/src/main.js
+++ Vue/learn-example/webpack/src/main.js
@@ -6,5 +6,19 @@ var name = "Vue"
 console.log(sum(10, 20))
 
 require('./css/normal.css')
+require('./css/special.less')
+
+document.writeln("<h2>Less</h2>")
+
+import Vue from 'vue'
+import App from './vue/App.vue'
+
+const app = new Vue({
+  el: "#app",
+  template: "<App></App>",
+  components: {
+    App
+  }
+})
 
 // webpack ./src/main.js -o ./dist/bundle.js --mode development
--- Vue/learn-example/webpack/webpack.config.js
+++ Vue/learn-example/webpack/webpack.config.js
@@ -1,17 +1,64 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/main.js',
   output: {
     path : path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: 'dist/'
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'img/[name].[hash:8].[ext]'
+            },
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
       }
     ]
+  },
+  plugins: [
+    // 请确保引入这个插件！
+    new VueLoaderPlugin()
+  ],
+  resolve: {
+    alias: {
+      'vue$':'vue/dist/vue.esm.js'
+    }
   }
 }
