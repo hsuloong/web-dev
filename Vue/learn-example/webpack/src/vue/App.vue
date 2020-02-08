aef822bc23403141ba91fb2de4e25490ff7d8d02
hsuloong <hsuloong@outlook.com>
2020-02-09 00:24:22 +0800

    add .vue

---
+++ Vue/learn-example/webpack/src/vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return { message: "Hi, Vue" }
+    }
+}
+</script>
